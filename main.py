e74270250ff844402d83be0aff4894ed42df5c8b
shany000 <ivan.shanygin@gmail.com>
2024-09-27 19:39:14 +0300

    google mandarinka2

--- main.py
+++ main.py
@@ -1,5 +1,34 @@
 import google
+import os
+import queue
+import sounddevice as sd
+import vosk
 
-s = input()
+def recognize_speech_vosk():
+    model_path = "models/vosk-model-small-ru-0.22"
+    if not os.path.exists(model_path):
+        print(f"Модель по пути '{model_path}' не найдена.")
+        return
 
-google.google_func(s)
+    model = vosk.Model(model_path)
+    audio_queue = queue.Queue()
+    sample_rate = 16000
+
+    def callback(indata, frames, time, status):
+        if status:
+            print(status, flush=True)
+        audio_queue.put(bytes(indata))
+
+    with sd.RawInputStream(samplerate=sample_rate, blocksize=8000, dtype='int16',
+                           channels=1, callback=callback):
+        print("Говорите, я вас слушаю...")
+
+        recognizer = vosk.KaldiRecognizer(model, sample_rate)
+
+        while True:
+            data = audio_queue.get()
+            if recognizer.AcceptWaveform(data):
+                result = recognizer.Result()
+                text = result.split('"text" : ')[-1].strip('}').replace('"', '').strip()
+                print(f"Вы сказали: {text}")
+recognize_speech_vosk()
